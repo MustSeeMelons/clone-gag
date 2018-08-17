18a7c3df46adff146217db56e4909ff7a943f701
Strautins <strautins.janis@gmail.com>
2018-08-17 18:18:31 +0300

    DB WIP.

--- src/main/resources/sql/config/init.sql
+++ src/main/resources/sql/config/init.sql
@@ -5,4 +5,19 @@ GRANT ALL PRIVILEGES ON DATABASE clone_gag TO strautins;
 
 \connect clone_gag;
 CREATE SCHEMA gag;
-CREATE TABLE gag.test(id int);
+CREATE TABLE gag.APP_USER(
+    id bigint SERIAL PRIMARY KEY,
+    sso_id varchar(30),
+    password varchar(30),
+    user_name varchar(30)
+);
+
+CREATE TABLE gag.USER_PROFILE(
+    id bigint SERIAL PRIMARY KEY,
+    type varchar(30) NOT NULL UNIQUE
+);
+
+CREATE TABLE gag.APP_USER_PROFILE (
+    user_id bigint REFERENCES gag.APP_USER(id),
+    user_proifle_id bigint REFERENCES gag.USER_PROFILE(id)
+);
