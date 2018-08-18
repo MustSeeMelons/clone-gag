35eb5eaf5698c0d31105469d32f4fcb70fde1612
Strautins <strautins.janis@gmail.com>
2018-08-18 21:28:18 +0300

    Fix SQL and docker file.

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM tomcat:9.0
 MAINTAINER strautins.janis@gmail.com
 
 COPY target/cloneGag.war /usr/local/tomcat/webapps/
-COPY Spring4MVCHelloWorldNoXMLDemo.war /usr/local/tomcat/webapps/
+
 # So that we can access manager
 COPY tomcat-users.xml /usr/local/tomcat/conf/
 
--- src/main/resources/sql/config/init.sql
+++ src/main/resources/sql/config/init.sql
@@ -4,16 +4,18 @@ CREATE USER strautins WITH ENCRYPTED PASSWORD 'PieciKabaci1';
 GRANT ALL PRIVILEGES ON DATABASE clone_gag TO strautins;
 
 \connect clone_gag;
+
 CREATE SCHEMA gag;
+
 CREATE TABLE gag.APP_USER(
-    id bigint SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     sso_id varchar(30),
     password varchar(30),
     user_name varchar(30)
 );
 
 CREATE TABLE gag.USER_PROFILE(
-    id bigint SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     type varchar(30) NOT NULL UNIQUE
 );
 
